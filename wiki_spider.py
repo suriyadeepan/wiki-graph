1df72460ba634cac78eb763b9a2a9bc36066d9fd
Suriyadeepan Ram <suriyadeepan.r@gmail.com>
2016-08-06 19:46:33 +0530

    spider picks up : {url, title, external url, external url title}

--- wiki_spider.py
+++ wiki_spider.py
@@ -6,21 +6,25 @@ _BASE_URL = 'https://en.wikipedia.org'
 
 class WikiSpider(scrapy.Spider):
     name = 'wiki'
-    start_urls = ['https://en.wikipedia.org/wiki/Rio_de_Janeiro']
+    start_urls = ['https://en.wikipedia.org/wiki/Transhumanism']
 
     def parse(self,response):
-        title = response.xpath('//h1[contains(@class,"firstHeading")]/text()').extract_first()
-        soup = BeautifulSoup(response,'lxml')
-        urls = []
-        titles = []
-        for url_tag in response.xpath("//a"):
-            href = url_tag.xpath("@href").extract_first()
-            if href.startswith('/wiki/') and not 'File' in href:
-                urls.append(_BASE_URL + href)
-                titles.append(url_tag.xpath('@title').extract_first())
-                
+        self_title = response.xpath('//h1[contains(@class,"firstHeading")]/text()').extract_first()
         self_url = response.url
+        soup = BeautifulSoup(response.body,'lxml')
+        # focus on links from paragraphs
+        link_tags = []
+        for p in soup.findAll('p'):
+            link_tags.extend(p.findAll('a'))
+        # find all the external links and corresponding titles
+        ext_links  = []
+        ext_titles = []
+        for tag in link_tags:
+            if not any( x in str(tag) for x in ['cite_note','File']):
+                if 'title' in tag.attrs and 'href' in tag.attrs:
+                    ext_titles.append(tag['title'])
+                    ext_links.append(_BASE_URL + tag['href'])
         
-        for url,ext_title in zip(urls,titles):
-            yield { 'title' : title, 'self_url' : self_url, 'ext_url' : url , 'ext_title' : ext_title}
-            yield scrapy.Request(url, callback=self.parse)
+        for ext_link, ext_title in zip(ext_links,ext_titles):
+            yield { 'title' : self_title, 'self_url' : self_url, 'ext_url' : ext_link , 'ext_title' : ext_title}
+            yield scrapy.Request(ext_link, callback=self.parse)
